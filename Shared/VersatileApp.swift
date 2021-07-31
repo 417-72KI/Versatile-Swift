04478bd69915bfb39633849bb0abc358197f93b0
417-72KI <417.72ki@gmail.com>
2021-07-31 15:16:23 +0900

    Initial Commit

---
+++ Shared/VersatileApp.swift
@@ -0,0 +1,17 @@
+//
+//  VersatileApp.swift
+//  Shared
+//
+//  Created by 417.72KI on 2021/07/31.
+//
+
+import SwiftUI
+
+@main
+struct VersatileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
